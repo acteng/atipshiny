1f28d04a3637a370d7fe1ad1b8f9d562f9aea462
Robin Lovelace <rob00x@gmail.com>
2022-10-08 00:37:00 +0100

    Working prototype

--- app.R
+++ app.R
@@ -16,10 +16,17 @@ ui = bs4DashPage(fullscreen = FALSE, dark = TRUE,
     width = "50%", # Changing width makes body fail
     minified = FALSE,
     textInput(inputId = "name", label = "Intervention name", value = "E.g. Chapeltown Active Travel Neighbourhood"),
-    textInput(inputId = "promoter", label = "Promoter", value = "Transport for ..."),
-    sliderInput(inputId = "trips_without", label = "Number of trips per day without the proposed intervention", min = 0, max = 1000, value = 100),
-    sliderInput(inputId = "trips_with", label = "Number of trips per day with the proposed intervention", min = 0, max = 1000, value = 110),
-    sliderInput(inputId = "uptake", label = "Uptake", min = 0, max = 100, value = 5),
+    textInput(inputId = "description", label = "Description", value = "Brief description of scheme"),
+    dateInput(inputId = "completion_date", label = "Planned completion date"),
+    shinyWidgets::currencyInput(inputId = "budget_capital", label = "Budget (capital)", format = "British", value = 10000),
+    shinyWidgets::currencyInput(inputId = "budget_annual", label = "Budget (revenue)", format = "British", value = 1000),
+    sliderInput(inputId = "impacts", label = "Network impacts", min = 1, max = 5, value = 3),
+    sliderInput(inputId = "porosity", label = "Porosity", min = 1, max = 5, value = 3),
+    sliderInput(inputId = "density", label = "Mesh density", min = 1, max = 5, value = 3),
+    sliderInput(inputId = "permeability", label = "Permeability", min = 1, max = 5, value = 3),
+    sliderInput(inputId = "motor", label = "Motor through-traffic", min = 1, max = 5, value = 3),
+    sliderInput(inputId = "crossings", label = "Crossings", min = 1, max = 5, value = 3),
+    sliderInput(inputId = "engagement", label = "Engagement practices", min = 1, max = 5, value = 3),
     div(style="position:relative; left:calc(25%);",     downloadButton("downloadData", "Download"))
   ),
   bs4DashBody(
@@ -42,9 +49,23 @@ server = function(input, output) {
     },
     content = function(file) {
       geom <- edits()$finished
-      
-            sf::write_sf(geom, file, delete_layer = TRUE, delete_dsn = TRUE)
-
+      geom = sf::st_sf(
+        data.frame(
+          name = input$name,
+          description = input$description,
+          completion_date = input$completion_date,
+          budget_capital = input$budget_capital,
+          budget_revenue = input$budget_revenue,
+          impacts = input$impacts,
+          porosity = input$porosity,
+          permeability = input$permeability,
+          motor = input$motor,
+          crossings = input$crossings,
+          engagement = input$engagement
+          ),
+        geometry = geom$geometry
+      )
+      sf::write_sf(geom, file, delete_layer = TRUE, delete_dsn = TRUE)
     }
   )
   
