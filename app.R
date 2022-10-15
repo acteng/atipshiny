0ad658c009281488264502faf2668e1b152d5adc
Robin Lovelace <rob00x@gmail.com>
2022-10-15 21:35:13 +0100

    Style improvements

--- app.R
+++ app.R
@@ -7,7 +7,7 @@ library(DT)
 
 dt_output = function(title, id) {
   fluidRow(column(
-    12, 
+    12,
     h3(title),
     hr(),
     DTOutput(id)
@@ -20,7 +20,7 @@ render_dt = function(data, editable = 'cell', server = TRUE, ...) {
 
 region = ukboundaries::leeds
 map = leaflet(data = region) %>%
-  addTiles() %>% 
+  addTiles() %>%
   addPolylines()
 
 ui = bs4DashPage(fullscreen = FALSE, dark = FALSE,
@@ -29,7 +29,7 @@ ui = bs4DashPage(fullscreen = FALSE, dark = FALSE,
     width = "40%", # Changing width makes body fail
     minified = FALSE,
     checkboxInput(inputId = "edit", label = "Map edits complete", value = FALSE),
-    conditionalPanel(condition = "input.edit == true", dt_output('Add data', 'x6')),
+    conditionalPanel(condition = "input.edit == true", dt_output('Add data', 'd_edit')),
     conditionalPanel(condition = "input.edit == true", div(style="position:relative; left:calc(25%);",     downloadButton("downloadData", "Download")))
   ),
   bs4DashBody(
@@ -45,23 +45,23 @@ server = function(input, output) {
     leafmap = map,
     id = "map"
   )
-  
+
   d_sf = sf::read_sf("intervention.geojson")
   d_df = sf::st_drop_geometry(d_sf)
-  
+
   observeEvent(input$edit, {
     geom = edits()$finished
     nrows <<- length(geom$geometry)
     d_df <<- d_df[rep(1, max(1, nrows)), ]
-    output$x6 = render_dt(d_df, 'row')
+    output$d_edit = render_dt(d_df, editable = 'all')
   })
-  
+
   # edit a row
-  observeEvent(input$x6_cell_edit, {
-    d_df <<- editData(d_df, input$x6_cell_edit, 'x6')
+  observeEvent(input$d_edit_cell_edit, {
+    d_df <<- editData(d_df, input$d_edit_cell_edit, 'd_edit')
     message(d_df$name, "2")
   })
-  
+
   output$downloadData = downloadHandler(
     filename = function() {
       paste("interventions", ".geojson", sep = "")
