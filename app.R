771ac318e2ef67deb984f817a06107c03e13a5ce
Robin Lovelace <rob00x@gmail.com>
2022-10-15 23:38:34 +0100

    Start with national view

--- app.R
+++ app.R
@@ -4,6 +4,26 @@ library(sf)
 library(mapedit)
 library(leaflet)
 library(DT)
+library(shinyjs)
+
+tas = sf::read_sf("https://github.com/udsleeds/openinfra/raw/main/data-small/transport_regions_2022.geojson")
+tool_types = c("Areas", "Routes (not yet implemented)", "Crossings (not yet implemented)")
+# Tool types
+task = c("Area tool: Draw the boundaries representing area interventions and click 'Map edits complete' in the togglable left hand sidebar when complete")
+data_input_instructions = paste(collapse = " ",
+  "Zoom into your region and draw the boundaries of area interventions",
+  "with the 'Draw a polygon' tool below the line 'Draw a polyline' tool.",
+  "Complete each boundary by clicking on the first boundary point.",
+  "When your areas are complete click on 'Map edits complete' above",
+  "and enter the name, description and other details of each intervention",
+  "in the table that appears by double clicking on the table.",
+  "Save the data by pressing Ctrl+Enter and download the data by clicking",
+  "the Download button. See demo video here (TBC)."
+)
+
+map = leaflet() |>
+  addTiles() |>
+  addPolylines(data = tas)
 
 dt_output = function(title, id) {
   fluidRow(column(
@@ -17,35 +37,46 @@ render_dt = function(data, editable = 'cell', server = TRUE, ...) {
   renderDT(data, selection = 'none', server = server, editable = editable, ...)
 }
 
-
-region = ukboundaries::leeds
-map = leaflet(data = region) %>%
-  addTiles() %>%
-  addPolylines()
-
-ui = bs4DashPage(fullscreen = FALSE, dark = FALSE,
-  dashboardHeader(title = "ATIP"),
-  dashboardSidebar(collapsed = TRUE, skin = "gray",
-    width = "40%", # Changing width makes body fail
+ui = bs4DashPage(
+  header = dashboardHeader(
+    title = "ATIP",
+    leftUi = dropdownMenu(task, type = "tasks")),
+  fullscreen = FALSE,
+  dark = FALSE,
+  title = "ATIP",
+  dashboardSidebar(
+    collapsed = TRUE,
+    skin = "gray",
+    width = "40%",
     minified = FALSE,
+    conditionalPanel(
+      condition = "input.edit == false",
+      selectInput(inputId = "tool", label = "Select intervention type", choices = tool_types),
+    ),
     checkboxInput(inputId = "edit", label = "Map edits complete", value = FALSE),
+    conditionalPanel(
+      condition = "input.edit == false",
+      textOutput("text")
+      ),
     conditionalPanel(condition = "input.edit == true", dt_output('Add data', 'd_edit')),
     conditionalPanel(condition = "input.edit == true", div(style="position:relative; left:calc(25%);",     downloadButton("downloadData", "Download")))
   ),
   bs4DashBody(
-    fillPage(
-      editModUI("map", width = "100%", height = 800)
-    )
+ editModUI("map", width = "100%", height = 800)
+    # fillPage(
+    #   )
     )
   )
 
 server = function(input, output) {
+  
   edits = callModule(
     editMod,
     leafmap = map,
     id = "map"
   )
 
+  output$text = renderText(data_input_instructions)
   d_sf = sf::read_sf("intervention.geojson")
   d_df = sf::st_drop_geometry(d_sf)
 
